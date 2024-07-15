4682bfc45b92879f2841703ab26eec66998a99e5
Soham Biswas <77766886+Sohambiswas8@users.noreply.github.com>
2024-07-15 08:36:01 +0530

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+# install conda
+wget https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+bash Anaconda3-2024.06-1-Linux-x86_64.sh
+
+conda init
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
+conda install bcftools samtools bwe gatk4 fastqc multiqc fastp
+
+# LDtect
+virtualenv ldetect
+source ldetect/bin/activate
+pip install ldetect
+
+# snpflip
+virtualenv snpflip
+source snpflip/bin/activate
+pip install snpflip
